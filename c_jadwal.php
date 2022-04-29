d0e5ce3e3c57f1a79d05af603e2d3ee5e5e999c3
ratnapinasti25 <86191077+ratnapinasti25@users.noreply.github.com>
2022-04-30 05:04:21 +0700

    Update c_jadwal.php

--- c_jadwal.php
+++ c_jadwal.php
@@ -3,22 +3,24 @@
     include_once("m_jadwal.php");
 
     if (isset($_POST['submit'])) {
-        $hari = $_POST['hari'];
         $tanggal = $_POST['tanggal'];
         $club1 = $_POST['club1'];
         $club2 = $_POST['club2'];
-        $control = new c_jadwal($hari, $tanggal, $club1, $club2);
+        $control = new c_jadwal($tanggal, $club1, $club2);
         $control->insert();
         echo "<script>location.href = 'jadwal.php'</script>";
     }
-
     class c_jadwal{
         public $model;
-        public function __construct($hari, $tanggal, $club1, $club2){
-        $this->model = new m_jadwal($hari, $tanggal, $club1, $club2);
+        public function __construct($tanggal, $club1, $club2){
+        $this->model = new m_jadwal($tanggal, $club1, $club2);
         }
         public function insert() {
             $this->model->setJadwal();
             Include 'jadwal.php';
+        }
+        public function invoke(){
+            $jadwal = $this->model->getJadwal();
+            include 'jadwal.php';
         }
     }
